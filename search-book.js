e2bd1f95770342f7dc0e5b409d9ad064890f8957
smllygrl <hodgsonatalie@gmail.com>
2021-10-09 17:07:31 +1100

    rendered all info on to page, no styling

--- search-book.js
+++ search-book.js
@@ -9,22 +9,54 @@ const accessSearchResults = async (searchInput) => {
 
 searchBtn.addEventListener("click", async (event) => {
   console.log("Search Button Pressed");
+
   const userInput = document.querySelector("#searchInput").value;
 
+  // bookResults is an array
   const booksResults = await accessSearchResults(userInput);
+  console.log(booksResults[0].volumeInfo);
+
+  // listofBooks is an array
   const listOfBooks = booksResults.map((book) => {
-    console.log(book.volumeInfo.title);
-    const titlePara = document.createElement("p");
-    const titleText = document.createTextNode(`${book.volumeInfo.title}`);
-    titlePara.appendChild(titleText);
-    return titlePara;
+    console.log(book.volumeInfo);
+
+    const bookObject = {
+      title: book.volumeInfo.title,
+      author: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      img: book.volumeInfo.imageLinks.thumbnail,
+    };
+    console.log(bookObject);
+    // let bookObjectArray = bookObject;
+    // console.log(bookObjectArray);
+
+    const bookDiv = document.createElement("div");
+    const bookDivContent = document.createTextNode(
+      `Title: ${bookObject.title} 
+      Author(s):${bookObject.author}
+      Description: ${bookObject.description}`
+    );
+    bookDiv.appendChild(bookDivContent);
+
+    const bookImage = document.createElement("img");
+    bookImage.src = bookObject.img;
+
+    bookDiv.appendChild(bookImage);
+
+    // const titlePara = document.createElement("p");
+    // const titleText = document.createTextNode(
+    //   `Title: ${book.volumeInfo.title}`
+    // );
+    // titlePara.appendChild(titleText);
+
+    return bookDiv;
   });
 
   // For each book we want to create a div
   // Inside each div we want a title, an image, a para
   // Append each of the book div to the book container
 
-  const bookContainer = document.querySelector(".bookContainer__search");
+  const bookContainer = document.querySelector(".bookContainer__results");
 
   listOfBooks.forEach((para) => {
     bookContainer.appendChild(para);
@@ -32,3 +64,11 @@ searchBtn.addEventListener("click", async (event) => {
 
   console.log(listOfBooks);
 });
+
+// const authorPara = document.createElement("p");
+// const authorText = document.createTextNode(
+//   `Authors: ${book.volumeInfo.authors}`
+// );
+// authorPara.appendChild(authorText);
+
+// console.log(`${book.volumeInfo.authors}`);
